fd686c9c870faba32c9fce4219ff03dcebc93b03
Arezoo-Nasr <85000769+Arezoo-Nasr@users.noreply.github.com>
2021-07-05 17:37:43 -0400

    Update sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -12,7 +12,7 @@ var allClasses = [];
 var allInterfaces = [];
 
 var filePaths = ["../**/resources/**.ts",
-]; aaa
+]; aaabbb
 
 try {
     const sourceFiles = project.addSourceFilesAtPaths(filePaths);
