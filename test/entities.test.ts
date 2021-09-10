019fc894936abd12634c807457744ce2d50d5402
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-09-10 17:45:36 -0400

    npm test works (but terribly slow)

---
+++ test/entities.test.ts
@@ -0,0 +1,14 @@
+import { TS2Famix } from '../src/ts2famix';
+import 'jest-extended';
+
+describe('ts2famix', () => {
+    it("should generate json", async () => {
+        const filePaths = ["../**/resources/**.ts"];
+        const importer = new TS2Famix();
+
+        const fmxRep2 = importer.famixRepFromPath(filePaths);
+        const jsonOutput = fmxRep2.getJSON();
+        // very basic test for now
+        expect(jsonOutput).toMatch(/FamixTypeScript.Class/);
+    });
+});
