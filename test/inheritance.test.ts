80449660ae348005866748b94be7641ee39299e0
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-10-24 19:28:26 -0400

    test pour inheritance

--- test/inheritance.test.ts
+++ test/inheritance.test.ts
@@ -20,7 +20,7 @@ describe('ts2famix', () => {
 
     it("should contain a Fish class that's a subclass of Animal", async () => {
         const fishCls = parsedModel.filter(el => (el.FM3 == "FamixTypeScript.Class" && el.name == "Fish"))[0];
-        console.info(`fishCls = ${JSON.stringify(fishCls)}`)
-        expect(idToElementMap.get(fishCls.superclass.ref).name).toBe("Animal"); // peut-être pas le bon format de la définition de la superclasse
+        const inheritance = idToElementMap.get(fishCls.superInheritances[0].ref);
+        expect(idToElementMap.get(inheritance.superclass.ref).name).toBe("Animal"); // nom de la superclasse
     })
 });
