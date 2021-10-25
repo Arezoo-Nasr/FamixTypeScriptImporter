82fd05e4114b3ce55ed8c0eb0c7e77117ec60ef4
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-10-25 11:41:06 -0400

    Exemple avec interface

--- resources/Inheritance.ts
+++ resources/Inheritance.ts
@@ -4,4 +4,13 @@ namespace MyNamespace {
 	}
 	export class Fish extends Animal {
 	}
+	export interface Flyable {
+		fly() : void;
+	}
+	export class Bird extends Animal implements Flyable {
+		fly(): void {
+			throw new Error("Method not implemented.");
+		}
+	}
+
 }
