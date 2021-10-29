8c70087a0e595206db840dc68f4dfd71860b7cea
arezoo-nasr <arezoo.nasrollahzadeh@gmail.com>
2021-10-29 12:26:22 -0400

    TsModel

--- resources/TypeScriptModel.ts
+++ resources/TypeScriptModel.ts
@@ -1,31 +1,29 @@
-export namespace MyNamespace {
 
-	export class class1 {
-		protected projectName: string;
 
-		constructor() {
-			this.projectName = "Support for TypeScript";
+	export namespace MyNamespace {
+
+		export class class1 {
+			protected projectName: string;
+
+			constructor() {
+				this.projectName = "Support for TypeScript";
+			}
 		}
 
-		public getName(university: string): string {
-			var finalString: string = this.projectName + "in Moose" + university;
-			return finalString;
+		export interface interface1 {
+
 		}
 
-	}
+		export class class2 extends class1 implements interface1 {
 
-	export interface interface1 {
-		att1: string;
-	}
-
-	export class class2 extends class1 implements interface1 {
-		att1: string = "";
-		public func() {
-			var x = this.projectName;
+			public getName(university: string): string {
+				var finalString: string = this.projectName + "in Moose" + university;
+				return finalString;
+			}
 		}
 	}
-}
 
-function function1() {
-	var localVar = "hi";
-}
+	function function1() {
+		var localVar = "hi";
+	}
+
