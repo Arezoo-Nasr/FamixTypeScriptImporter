e82719fb314f886931ea94455b9bb53b5ea3212f
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-02-01 22:02:16 -0500

    clean up (remove async)

--- test/entities.test.ts
+++ test/entities.test.ts
@@ -1,6 +1,6 @@
 import { TS2Famix } from '../src/ts2famix';
 import 'jest-extended';
-import { BehaviouralEntity, Method, NamedEntity, SourcedEntity } from '../src/lib/famix/src/model/famix';
+import { Method } from '../src/lib/famix/src/model/famix';
 
 const filePaths = ["test_src/Entity.ts"];
 const importer = new TS2Famix();
@@ -9,26 +9,19 @@ const fmxRep2 = importer.famixRepFromPath(filePaths);
 const jsonOutput = fmxRep2.getJSON();
 let parsedModel: Array<any>;
 
-// const idToElementMap: Map<number, any> = new Map();
-// function initMapFromModel(model) {
-//     model.forEach(element => {
-//         idToElementMap.set(element.id, element);
-//     });
-// }
-
 describe('ts2famix', () => {
-    it("should generate valid json", async () => {
+    it("should generate valid json", () => {
         parsedModel = JSON.parse(jsonOutput);
         expect(parsedModel).toBeTruthy();
         // initMapFromModel(parsedModel);
     });
 
-    it("should generate json with FM3 FamixTypeScript.Class for EntityClass", async () => {
+    it("should generate json with FM3 FamixTypeScript.Class for EntityClass", () => {
         expect(jsonOutput)
             .toMatch(/"FM3":"FamixTypeScript.Class","id":[1-9]\d*|0,"sourceAnchor":{"ref":[1-9]\d*|0},"name":"EntityClass"/);
     });
 
-    it("should contain an EntityClass class with three methods: move, move2 and constructor", async () => {
+    it("should contain an EntityClass class with three methods: move, move2 and constructor", () => {
         const theClass = parsedModel.filter(el => (el.FM3 == "FamixTypeScript.Class" && el.name == "EntityClass"))[0];
         expect(theClass.methods.length).toBe(3);
         let mNames: Set<string> = new Set();
