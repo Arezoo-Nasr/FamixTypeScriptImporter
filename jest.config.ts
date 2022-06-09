4eb63c5536544894be8b010e0d719a54d427f0b5
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-06-08 23:04:59 -0400

    Speed up jest (2x)

--- jest.config.ts
+++ jest.config.ts
@@ -61,9 +61,14 @@ export default {
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  // maxWorkers: 1,
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
@@ -137,7 +142,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
