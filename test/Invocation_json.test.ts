c02d4450c657d6f83fae3ad0c5498f589767e036
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-06-10 12:01:58 -0400

    fix invocations test (json)

--- test/Invocation_json.test.ts
+++ test/Invocation_json.test.ts
@@ -23,39 +23,30 @@ describe('Invocation', () => {
     });
 
     it("should have one invocation for method returnHi in Class1",  () => {
-        const invocationCls = parsedModel.filter(el => (el.FM3 == "FamixTypeScript.Class" && el.name == "Class1"))[0];
-        const invocationClsMethods = parsedModel.filter(e => invocationCls.methods.some(m => m.ref == e.id));
-        const methodNames: string[] = ['returnHi'];
-        invocationClsMethods.forEach(m => expect(methodNames).toContain(m.name));
-        const foundMethods = invocationClsMethods.filter(m => methodNames.includes(m.name));
-        expect(foundMethods.length).toBe(1);
-        const checkMethodHasInvocation = foundMethods.every(m => m.candidates !== undefined);
-        expect(checkMethodHasInvocation).toBe(true);
-
-        invocationClsMethods.forEach(method => {
-            const invocationCls = parsedModel.filter(e => e.FM3 == "FamixTypeScript.Invocation"
-                && method.candidates.some(m => m.ref == e.id));
-            const checkHasRelatedToMethod = invocationCls.every(a => a.receiver !== undefined && a.receiver.ref == method.id);
-            expect(checkHasRelatedToMethod).toBe(true);
-        });
+        verifyInvocation("Class1", "returnHi");
     });
 
     it("should have one invocation for method returnName in Class2",  () => {
-        const invocationCls = parsedModel.filter(el => (el.FM3 == "FamixTypeScript.Class" && el.name == "Class2"))[0];
-        const invocationClsMethods = parsedModel.filter(e => invocationCls.methods.some(m => m.ref == e.id));
-        const methodNames: string[] = ['returnName'];
-        invocationClsMethods.forEach(m => expect(methodNames).toContain(m.name));
-        const foundMethods = invocationClsMethods.filter(m => methodNames.includes(m.name));
-        expect(foundMethods.length).toBe(1);
-        const checkMethodHasInvocation = foundMethods.every(m => m.candidates !== undefined);
-        expect(checkMethodHasInvocation).toBe(true);
-
-        invocationClsMethods.forEach(method => {
-            const invocationCls = parsedModel.filter(e => e.FM3 == "FamixTypeScript.Invocation"
-                && method.candidates.some(m => m.ref == e.id));
-            const checkHasRelatedToMethod = invocationCls.every(a => a.receiver !== undefined && a.receiver.ref == method.id);
-            expect(checkHasRelatedToMethod).toBe(true);
-        });
+        verifyInvocation("Class2", "returnName");
     });
 
 })
+
+function verifyInvocation(theClass: string, theMethod: string) {
+    const invocationCls = parsedModel.filter(el => (el.FM3 == "FamixTypeScript.Class" && el.name == theClass))[0];
+    const invocationClsMethods = parsedModel.filter(e => invocationCls.methods.some(m => m.ref == e.id));
+    const methodNames: string[] = [theMethod];
+    invocationClsMethods.forEach(m => expect(methodNames).toContain(m.name));
+    const foundMethods = invocationClsMethods.filter(m => methodNames.includes(m.name));
+    expect(foundMethods.length).toBe(1);
+    const checkMethodHasInvocation = foundMethods.every(m => m.incomingInvocations !== undefined);
+    expect(checkMethodHasInvocation).toBe(true);
+
+    invocationClsMethods.forEach(method => {
+        const invocationCls = parsedModel.filter(e => e.FM3 == "FamixTypeScript.Invocation"
+            && method.incomingInvocations.some(m => m.ref == e.id));
+        const checkHasRelatedToMethod = invocationCls.every(a => a.candidates.length && a.candidates[0].ref == method.id);
+        expect(checkHasRelatedToMethod).toBe(true);
+    });
+}
+
--- test/generics.test.ts
+++ test/generics.test.ts
@@ -1,5 +1,5 @@
 import { TS2Famix } from '../src/ts2famix';
-import { Exception, ParameterizableClass } from '../src/lib/famix/src/model/famix';
+import { ParameterizableClass } from '../src/lib/famix/src/model/famix';
 
 const filePaths = ["test_src/generics/**/*"];
 const importer = new TS2Famix();
