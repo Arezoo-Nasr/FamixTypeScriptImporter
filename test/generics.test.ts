a5b108ef8a5c96e0435ea35da15da8ce6f811607
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-06-12 19:08:42 -0400

    Improved generics test

--- test/generics.test.ts
+++ test/generics.test.ts
@@ -1,5 +1,5 @@
 import { TS2Famix } from '../src/ts2famix';
-import { ParameterizableClass } from '../src/lib/famix/src/model/famix';
+import { ParameterizableClass, ParameterizedType } from '../src/lib/famix/src/model/famix';
 
 const filePaths = ["test_src/generics/**/*"];
 const importer = new TS2Famix();
@@ -7,17 +7,20 @@ const importer = new TS2Famix();
 const fmxRep2 = importer.famixRepFromPath(filePaths);
 
 describe('generics ts2famix', () => {
-    it("should verify generics...", () => {
+    it("should parse generics.", () => {
         expect(fmxRep2).toBeTruthy()
     });
-    it("should contain a generic class", () => {
+    it("should contain a single generic class", () => {
         expect(fmxRep2.getAllEntitiesWithType("ParameterizableClass").length).toBe(1)
     });
     it("should contain a generic class MyDao", () => {
         expect((fmxRep2.getAllEntitiesWithType("ParameterizableClass")[0] as ParameterizableClass).getName()).toBe("MyDao")
     });
     it("should contain a generic class MyDao with a parameter type T", () => {
-        expect((fmxRep2.getAllEntitiesWithType("ParameterizableClass")[0] as ParameterizableClass).getParameterTypes().size).toBe(1)
+        const pClass = fmxRep2
+        .getAllEntitiesWithType("ParameterizableClass")[0] as ParameterizableClass;
+        expect(pClass.getParameterTypes().size).toBe(1);
+        expect((Array.from(pClass.getParameterTypes())[0] as ParameterizedType).getName()).toBe("T");
     });
 
 });
