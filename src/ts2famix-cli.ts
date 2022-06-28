66fc699883336a9fd425f70321f74be85042fd01
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-06-28 14:45:44 -0400

    upgrade ts-morph and ts

--- src/ts2famix-cli.ts
+++ src/ts2famix-cli.ts
@@ -2,13 +2,20 @@ import * as fs from "fs"
 import yargs from "yargs";
 import { TS2Famix } from "./ts2famix";
 
+// const argv = yargs
+//     .example('$0 -i ../myTypescriptProject -o myTypeScriptProject.json', 'creates JSON-format model of typescript project')
+//     .alias('i', 'input')
+//     .nargs('i', 1)
+//     .alias('o', 'output')
+//     .nargs('o', 1)
+//     .demandOption('input').demandOption('output').argv;
 const argv = yargs
     .example('$0 -i ../myTypescriptProject -o myTypeScriptProject.json', 'creates JSON-format model of typescript project')
     .alias('i', 'input')
     .nargs('i', 1)
     .alias('o', 'output')
     .nargs('o', 1)
-    .demandOption('input').demandOption('output').argv;
+    .demandOption('input').demandOption('output').parseSync();
 
 const importer = new TS2Famix();
 const paths = new Array<string>();
