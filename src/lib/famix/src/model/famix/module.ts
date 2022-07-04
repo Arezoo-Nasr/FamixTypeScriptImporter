563e271516c3e5a148414c0514cc2ddfa79d3f63
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-07-04 09:28:08 -0400

    Start to add variables to functions

--- src/lib/famix/src/model/famix/module.ts
+++ src/lib/famix/src/model/famix/module.ts
@@ -1,8 +1,9 @@
-// automatically generated code, please do not change
+// automatically generated code, please do not change (sorry, changed by C. Fuhrman)
 
 import { FamixJSONExporter } from "../../famix_JSON_exporter";
 import { CompilationUnit } from "./../famix/compilation_unit";
 import { ScopingEntity } from "./../famix/scoping_entity";
+import { LocalVariable } from "./local_variable";
 
 export class Module extends ScopingEntity {
 
@@ -20,6 +21,22 @@ export class Module extends ScopingEntity {
     }
   }
 
+  private scopingEntityLocalVariables: Set<LocalVariable> = new Set();
+
+  // manyOne.Getter
+  // @FameProperty(name = "localVariables", opposite = "parentBehaviouralEntity", derived = true)
+  public getLocalVariables(): Set<LocalVariable> {
+    return this.scopingEntityLocalVariables;
+  }
+
+  // manyOne.Setter
+  public addLocalVariables(scopingEntityLocalVariables: LocalVariable) {
+    if (!this.scopingEntityLocalVariables.has(scopingEntityLocalVariables)) {
+      this.scopingEntityLocalVariables.add(scopingEntityLocalVariables);
+      // TODO scopingEntityLocalVariables.setParentBehaviouralEntity(this);
+    }
+  }
+
 
   public getJSON(): string {
     const mse: FamixJSONExporter = new FamixJSONExporter("Module", this);
@@ -30,6 +47,7 @@ export class Module extends ScopingEntity {
   public addPropertiesToExporter(exporter: FamixJSONExporter) {
     super.addPropertiesToExporter(exporter);
     exporter.addProperty("compilationUnit", this.getCompilationUnit());
+    exporter.addProperty("localVariables", this.getLocalVariables());
 
   }
 
