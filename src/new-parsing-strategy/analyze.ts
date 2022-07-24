6190aa26d1db7c00af9d10a36285ec41e98bfcee
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2022-07-23 22:01:03 -0400

    Find classes, better info on output

--- src/new-parsing-strategy/analyze.ts
+++ src/new-parsing-strategy/analyze.ts
@@ -1,30 +1,66 @@
-import ts from "ts-morph";
+import ts, { ClassDeclaration, VariableStatement } from "ts-morph";
 import { Node, Statement, SyntaxKind, FunctionDeclaration, Project, VariableDeclaration } from "ts-morph";
 
 const declarations: VariableDeclaration[] = [];
 const project = new Project();
 project.addSourceFilesAtPaths("**/*.ts");
 
+const classes = new Array<ClassDeclaration>();
+const functions = new Array<FunctionDeclaration>();
+const variables = new Array<VariableDeclaration>();
+const variableStatements = new Array<VariableStatement>();
+
 const sourceFile = project.getSourceFileOrThrow("sample.ts");
 
+// For each element scanned for at the sourceFile, it should also be scanned for at the module level.
+
+console.info(`----------Finding Classes:`)
+sourceFile.getClasses().forEach(c => processClass(c));
+
+console.info(`----------Finding VariableStatements:`)
 sourceFile.getVariableStatements().forEach(v => processVariableStatement(v));
 
+console.info(`----------Finding VariableDeclarations:`)
 sourceFile.getVariableDeclarations().forEach(v => processVariable(v));
 
+console.info(`----------Finding Functions:`)
 sourceFile.getFunctions().forEach(f => processFunction(f));
 
+console.info(`----------Finding Modules:`)
 sourceFile.getModules().forEach(m => {
-    console.log("Modules:");
+    console.info(`Module "${m.getName()}":`)
+    console.info(`----------Finding Classes in Module "${m.getName()}":`)
+    m.getClasses().forEach(c => processClass(c));
+    console.info(`----------Finding VariableStatements in Module "${m.getName()}":`)
     m.getVariableStatements().forEach(v => processVariableStatement(v));
+    console.info(`----------Finding VariableDeclarations in Module "${m.getName()}":`)
     m.getVariableDeclarations().forEach(v => processVariable(v));
+    console.info(`----------Finding Functions in Module "${m.getName()}":`)
     m.getFunctions().forEach(f => processFunction(f));
 });
 
+console.info(`\nClasses:`);
+classes.forEach(c => console.log(c.getName()))
+console.info(`\nVariables:`);
+variables.forEach(v => console.log(v.getName()))
+console.info(`\nVariable Statements:`);
+variableStatements.forEach(vs => console.log(vs.getDeclarations()[0].getName()))
+console.info(`\nFunctions:`);
+functions.forEach(f => console.info(f.getName()))
+
+
+function processClass(c: ClassDeclaration): void {
+    classes.push(c);
+    console.log(`class: ${c.getName()} (${c.getType().getText()}), fqn = ${c.getSymbol()?.getFullyQualifiedName()}`)
+}
+
 function processVariable(v: VariableDeclaration): void {
+    variables.push(v);
     console.log(`variable: ${v.getName()} (${v.getType().getText()}), fqn = ${v.getSymbol()?.getFullyQualifiedName()}`)
 }
 
 function processFunction(f: FunctionDeclaration): void {
+    functions.push(f);
     console.log(`function: ${f.getName()}`);
     f.getVariableStatements().forEach(v => processVariableStatement(v));
     f.getVariableDeclarations().forEach(v => processVariable(v));
@@ -52,5 +88,6 @@ function processStatements(statements: Statement[]) {
 }
 
 function processVariableStatement(v: ts.VariableStatement): void {
-    console.log(`Variable statement: ${v.getDeclarationKind()} ${v.getDeclarationKindKeyword().getText()} ${v.getDeclarations()[0].getName()}`)
+    variableStatements.push(v);
+    console.log(`Variable statement: ${v.getDeclarationKindKeyword().getText()} ${v.getDeclarations()[0].getName()}`)
 }
--- src/new-parsing-strategy/sample.ts
+++ src/new-parsing-strategy/sample.ts
@@ -1,7 +1,22 @@
 var x = "hello";
 
+class A {
+    aFred: number;
+    aWilma: "green" | "blue" | 12;
+    aMethod1(aMethodArg1: string) {
+        return aMethodArg1 + "!"
+    }    
+}
+
 namespace A {
     var w = 15;
+    class B {
+        bFred: number;
+        bWilma: "yellow" | "red";
+        bMethod1(bMethodArg1: number) {
+            return bMethodArg1 + 1;
+        }
+    }
 }
 
 export const y = 12.5;
@@ -19,6 +34,31 @@ function f() {
     try {
         var tryBlockL = 1;
     } catch (error) {
-        var catchBlockL = 12;        
+        var catchBlockL = 12;
+    }
+    g();
+
+    function g() {
+        var a_G: string;
+        h()
+
+        function h() {
+            var a_H: string;
+            i();
+
+            function i() {
+                var a_I: string;
+
+                console.info("Hello")
+
+                try {
+                    let tryX = 0;
+                } catch (error) {
+                    let catchY = 15;
+                } finally {
+                    let finallyZ = 99;
+                }
+            }
+        }
     }
 }
