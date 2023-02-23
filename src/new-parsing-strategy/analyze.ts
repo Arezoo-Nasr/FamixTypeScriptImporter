e595d0aced068904ac5211792b99a347e33820a6
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-02-23 00:20:19 -0500

    hacked a FQN function (using ancestors)

--- src/new-parsing-strategy/analyze.ts
+++ src/new-parsing-strategy/analyze.ts
@@ -1,4 +1,4 @@
-import ts, { ClassDeclaration, VariableStatement } from "ts-morph";
+import ts, { ClassDeclaration, MethodDeclaration, VariableStatement } from "ts-morph";
 import { Node, Statement, SyntaxKind, FunctionDeclaration, Project, VariableDeclaration } from "ts-morph";
 
 const declarations: VariableDeclaration[] = [];
@@ -9,6 +9,8 @@ const classes = new Array<ClassDeclaration>();
 const functions = new Array<FunctionDeclaration>();
 const variables = new Array<VariableDeclaration>();
 const variableStatements = new Array<VariableStatement>();
+const methods = new Array<MethodDeclaration>();
+//const fields = new Array<>();
 
 const sourceFile = project.getSourceFileOrThrow("sample.ts");
 
@@ -28,25 +30,27 @@ sourceFile.getFunctions().forEach(f => processFunction(f));
 
 console.info(`----------Finding Modules:`)
 sourceFile.getModules().forEach(m => {
-    console.info(`Module "${m.getName()}":`)
+    console.info(`Module "${m.getName()}", fqn = '${m.getSourceFile().getBaseName()}':`)
     console.info(`----------Finding Classes in Module "${m.getName()}":`)
     m.getClasses().forEach(c => processClass(c));
-    console.info(`----------Finding VariableStatements in Module "${m.getName()}":`)
-    m.getVariableStatements().forEach(v => processVariableStatement(v));
     console.info(`----------Finding VariableDeclarations in Module "${m.getName()}":`)
     m.getVariableDeclarations().forEach(v => processVariable(v));
+    console.info(`----------Finding VariableStatements in Module "${m.getName()}":`)
+    m.getVariableStatements().forEach(v => processVariableStatement(v));
     console.info(`----------Finding Functions in Module "${m.getName()}":`)
     m.getFunctions().forEach(f => processFunction(f));
 });
 
-console.info(`\nClasses:`);
-classes.forEach(c => console.log(c.getName()))
-console.info(`\nVariables:`);
-variables.forEach(v => console.log(v.getName()))
-console.info(`\nVariable Statements:`);
-variableStatements.forEach(vs => console.log(vs.getDeclarations()[0].getName()))
-console.info(`\nFunctions:`);
-functions.forEach(f => console.info(f.getName()))
+// console.info(`\nClasses:`);
+// classes.forEach(c => console.log(c.getName()))
+// console.info(`\nMethods:`);
+// methods.forEach(m => console.log(`${m.getParent().asKind(SyntaxKind.ClassDeclaration).getName()}.${m.getName()}`))
+// console.info(`\nVariables:`);
+// variables.forEach(v => console.log(`(${v.getParent().getParent().getParent().getKindName()}).${v.getName()}`))
+// console.info(`\nVariable Statements:`);
+// variableStatements.forEach(vs => console.log(vs.getDeclarations()[0].getName()))
+// console.info(`\nFunctions:`);
+// functions.forEach(f => console.info(f.getName()))
 
 
 function processClass(c: ClassDeclaration): void {
@@ -55,11 +59,14 @@ function processClass(c: ClassDeclaration): void {
     // Créer l'élément FAMIX ?
 
     console.log(`class: ${c.getName()} (${c.getType().getText()}), fqn = ${c.getSymbol()?.getFullyQualifiedName()}`)
+    c.getMethods().forEach(m => processMethod(m));
+    // c.getConstructors().forEach(con => processConstructor(con));
+    // c.getMembers().forEach(mem => processMember(mem));
 }
 
 function processVariable(v: VariableDeclaration): void {
     variables.push(v);
-    console.log(`variable: ${v.getName()} (${v.getType().getText()}), fqn = ${v.getSymbol()?.getFullyQualifiedName()}`)
+    console.log(`variable declaration: ${v.getName()} (${v.getType().getText()}), fqn = ${v.getSymbol()?.getFullyQualifiedName()} ${v.getInitializer() ? "initializer: '" + v.getInitializer().getText() + "'" : ''} `);
 }
 
 function processFunction(f: FunctionDeclaration): void {
@@ -74,10 +81,18 @@ function processFunction(f: FunctionDeclaration): void {
 function processStatements(statements: Statement[]) {
     for (const statement of statements) {
         console.log(`processing statements...`);
+        // the following can contain VariableDeclaration
         if (Node.isIfStatement(statement) 
             || Node.isDoStatement(statement) 
             || Node.isTryStatement(statement) 
-            || Node.isCatchClause(statement)
+            || Node.isCatchClause(statement) // vd appear in TryStatement
+            || Node.isBlock(statement)
+            || Node.isForInStatement(statement)
+            || Node.isForOfStatement(statement)
+            || Node.isForStatement(statement)
+            || Node.isSwitchStatement(statement)
+            || Node.isWhileStatement(statement)
+            || Node.isVariableStatement(statement)
             /* etc.*/ ) {
             console.log(`variables in ${statement.getKindName()}`);
             statement.getDescendantsOfKind(SyntaxKind.VariableDeclaration).forEach(vd => {
@@ -92,5 +107,12 @@ function processStatements(statements: Statement[]) {
 
 function processVariableStatement(v: ts.VariableStatement): void {
     variableStatements.push(v);
-    console.log(`Variable statement: ${v.getDeclarationKindKeyword().getText()} ${v.getDeclarations()[0].getName()}`)
+    console.log(`Variable statement: ${v.getDeclarationKindKeyword().getText()} '${v.getText()}' ${v.getDeclarations()[0].getName()}`)
 }
+
+function processMethod(m: ts.MethodDeclaration): void {
+    methods.push(m);
+    console.log(`Method declaration: ${m.getName()}`);
+    m.getFunctions().forEach(fun => processFunction(fun));
+}
+
--- src/new-parsing-strategy/sample.ts
+++ src/new-parsing-strategy/sample.ts
@@ -1,28 +1,52 @@
+
 var x = "hello";
 
-class A {
+export class ClassA {
     aFred: number;
     aWilma: "green" | "blue" | 12;
     aMethod1(aMethodArg1: string) {
         return aMethodArg1 + "!"
-    }    
+        function fInAMethod() {
+            function fInfInAMethod() {
+                var w = 0;
+            }
+        }
+}    
 }
 
-namespace A {
+namespace ANamespace {
     var w = 15;
+    let x;
+    x = w;
     class B {
         bFred: number;
         bWilma: "yellow" | "red";
         bMethod1(bMethodArg1: number) {
+            let varInBMethod;
             return bMethodArg1 + 1;
+            function fInBMethod() {
+                let varInFInBMethod;
+                function fInfInBMethod() {
+                    var w = 0;
+                }
+            }
         }
     }
+    let b = new B();
 }
 
 export const y = 12.5;
 
 function f() {
     var k = 0;
+    { var blockVar = 66; }
+    switch (k) {
+        case 0:
+            var kSwitchCase = 99;
+            break;
+        default:
+            var kSwitchDefault = 90;
+    }
     if (true) {
         var ifBlockL = 12;
     }
@@ -35,6 +59,22 @@ function f() {
         var tryBlockL = 1;
     } catch (error) {
         var catchBlockL = 12;
+    }
+    while (false) {
+        var kWhile = 9;
+    }
+    let array = [1, 2, 3];
+    for (let forIndex = 0; forIndex < array.length; forIndex++) {
+        const elementInFor = array[forIndex];
+    }
+    let object = {}
+    for (const key in object) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+            const elementInForIn = object[key];
+        }
+    }
+    for (const iterator of array) {
+        let elementInForOf = "blah"
     }
     g();
 
