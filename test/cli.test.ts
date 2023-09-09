1519ca23a44dee6c7219d59d83fd2381aea970fd
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-09-09 15:29:26 -0400

    test for command line

---
+++ test/cli.test.ts
@@ -0,0 +1,24 @@
+import { execSync } from 'child_process';
+
+const commandLineStart = 'ts-node src/ts2famix-cli.ts';
+
+describe('ts2famix should handle command line options', () => {
+    test('should show help option', () => {
+        try {
+            const output = execSync(commandLineStart, {
+                encoding: 'utf-8',
+            });
+            console.info("Output: " + output);
+        } catch (error) {
+            console.info("Error JSON: " + JSON.stringify(error));
+            expect(error.message).toContain(`--help`);
+        }
+    });
+
+    test('should show that a json was produced', () => {
+        const output = execSync(`${commandLineStart} -i "./test_src/**/*.ts" -o JSONModels/temp.json`, {
+            encoding: 'utf-8',
+        });
+        expect(output).toContain(`JSONModels/temp.json`);
+    });
+});
