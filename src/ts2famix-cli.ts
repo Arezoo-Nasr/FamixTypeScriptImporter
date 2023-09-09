af0a89c25c36e9f89360997363b548d9a2379d7e
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-09-09 12:58:46 -0400

    Fix npm command, support config files

--- src/ts2famix-cli.ts
+++ src/ts2famix-cli.ts
@@ -1,21 +1,34 @@
+#!/usr/bin/env node
 import * as fs from "fs";
 import yargs from "yargs";
 import { Importer } from './analyze';
-
-const importer = new Importer();
+import { FamixRepository } from "./lib/famix/src/famix_repository";
+import { Project } from "ts-morph";
 
 const argv = yargs
-    .example(`ts-node src/ts2famix-cli.ts -i "../path/to/project/**/*.ts" -o JSONModels/projectName.json`, 'creates a JSON-format model of a typescript project')
+    .example(`ts2famix -i "path/to/project/**/*.ts" -o JSONModels/projectName.json`, 'Creates a JSON-format Famix model of typescript files.')
+    .example(`ts2famix -i path/to/tsconfig.json -o JSONModels/projectName.json`, 'Creates a JSON-format model of a typescript project.')
     .alias('i', 'input')
     .nargs('i', 1)
     .alias('o', 'output')
     .nargs('o', 1)
     .demandOption('input').demandOption('output').parseSync();
 
-const paths = new Array<string>();
-paths.push(argv.input as string);
+const importer = new Importer();
+let famixRep: FamixRepository;
 
-const famixRep = importer.famixRepFromPaths(paths);
+if ((argv.input as string).endsWith('tsconfig.json')) {
+    const tsConfigFilePath = argv.input as string;
+    const project = new Project({
+        tsConfigFilePath
+      }); 
+    famixRep = importer.famixRepFromProject(project);
+} else {
+    const paths = new Array<string>();
+    paths.push(argv.input as string);
+    famixRep = importer.famixRepFromPaths(paths);
+}
+
 const jsonOutput = famixRep.getJSON();
 const jsonFilePath = argv.output as string;
 
