4d1621a5d31ebeca763520b882dc83450747fdaf
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-09-18 17:11:34 -0400

    test default import

--- test/importClause.test.ts
+++ test/importClause.test.ts
@@ -1,6 +1,6 @@
 import { Project } from "ts-morph";
 import { Importer } from "../src/analyze";
-import { ImportClause, Module, NamedEntity, Type } from "../src/lib/famix/src/model/famix";
+import { ImportClause, Module, NamedEntity } from "../src/lib/famix/src/model/famix";
 
 const importer = new Importer();
 const project = new Project();
@@ -11,20 +11,57 @@ project.createSourceFile("oneClassExporter.ts",
 project.createSourceFile("oneClassImporter.ts",
     `import {ExportedClass} from "./oneClassExport";`);
 
+project.createSourceFile("complexClassModule.ts",
+    `class ClassZ {}
+class ClassY {}
+export class ClassX {}
+
+export { ClassZ, ClassY };
+export { Importer } from '../src/analyze';
+
+export default class ClassW {}
+
+export namespace Nsp {}
+`);
+
+project.createSourceFile("defaultImporterModule.ts",
+    `import * as test from "./sampleForModule.ts";`);
+
 const fmxRep = importer.famixRepFromProject(project);
 
 describe('Tests for import clauses', () => {
-    it("should contain an import clause for ExportedClass", () => {
-        const importClauses = Array.from(fmxRep._getAllEntitiesWithType("ImportClause"));
+    it("should have four modules", () => {
+        const moduleList = Array.from(fmxRep._getAllEntitiesWithType('Module')) as Array<Module>;
+        expect(moduleList?.length).toBe(4);
+        const exporterModule = moduleList.find(e => e.getName() === 'oneClassExporter.ts');
+        expect(exporterModule).toBeTruthy();
+        const importerModule = moduleList.find(e => e.getName() === 'oneClassImporter.ts');
+        expect(importerModule).toBeTruthy();
+        const complexModule = moduleList.find(e => e.getName() === 'complexClassModule.ts');
+        expect(complexModule).toBeTruthy();
+    });
+
+    it("should have a default import clause for test", () => {
+        const importClauses = Array.from(fmxRep._getAllEntitiesWithType("ImportClause")) as Array<ImportClause>;
         expect(importClauses).toBeTruthy();
-        expect(importClauses.length).toBe(1);
-        const importClause = importClauses[0] as ImportClause;
+        expect(importClauses.length).toBe(2);
+        // find the import clause for ClassW
+        const importClause = importClauses.find(e => e.getImportedEntity()?.getName() === 'test');
         expect(importClause).toBeTruthy();
-        expect(importClause.getImportedEntity()).toBeTruthy();
-        expect(importClause.getImportedEntity()?.getName()).toBe("ExportedClass");
+        // importing entity is complexClassModule.ts
+        expect(importClause?.getImportingEntity()).toBeTruthy();
+        expect(importClause?.getImportingEntity()?.getName()).toBe("defaultImporterModule.ts");
+    });
+
+    it("should contain an import clause for ExportedClass", () => {
+        const importClauses = Array.from(fmxRep._getAllEntitiesWithType("ImportClause")) as Array<ImportClause>;
+        expect(importClauses).toBeTruthy();
+        expect(importClauses.length).toBe(2);
+        const importClause = importClauses.find(e => e.getImportedEntity()?.getName() === 'test');
+        expect(importClause).toBeTruthy();
         // importing entity is oneClassImporter.ts
-        expect(importClause.getImportingEntity()).toBeTruthy();
-        expect(importClause.getImportingEntity()?.getName()).toBe("oneClassImporter.ts");
+        expect(importClause?.getImportingEntity()).toBeTruthy();
+        expect(importClause?.getImportingEntity()?.getName()).toBe("defaultImporterModule.ts");
     });
 
     it("should contain one outgoingImports element for module oneClassImporter.ts", () => {
@@ -34,7 +71,7 @@ describe('Tests for import clauses', () => {
         expect(importerModule?.getOutgoingImports()).toBeTruthy();
         expect(importerModule?.getOutgoingImports()?.size).toBe(1);
         expect(importerModule?.getOutgoingImports()?.values().next().value.getImportedEntity()?.getName()).toBe("ExportedClass");
-    }); 
+    });
 
     it("should contain one imports element for module oneClassExporter.ts", () => {
         const entityList = Array.from(fmxRep._getAllEntitiesWithType('NamedEntity')) as Array<NamedEntity>;
@@ -42,5 +79,6 @@ describe('Tests for import clauses', () => {
         expect(exportedEntity).toBeTruthy();
         expect(exportedEntity?.getIncomingImports()).toBeTruthy();
         expect(exportedEntity?.getIncomingImports()?.size).toBe(1);
-        expect(exportedEntity?.getIncomingImports()?.values().next().value.getImportingEntity()?.getName()).toBe("oneClassImporter.ts");    }); 
+        expect(exportedEntity?.getIncomingImports()?.values().next().value.getImportingEntity()?.getName()).toBe("oneClassImporter.ts");
+    });
 });
