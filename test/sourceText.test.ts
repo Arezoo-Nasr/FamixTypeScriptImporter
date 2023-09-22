e3b6eaa24577493b0b2aab59ad2c4350ca974a16
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2023-09-21 22:28:04 -0400

    Fixes #23 Use grapheme-splitter (npm) package to get correct start and end for Moose despite emojis or other graphemes that are represented by multiple characters.

--- test/sourceText.test.ts
+++ test/sourceText.test.ts
@@ -1,6 +1,7 @@
 import { Project } from "ts-morph";
 import { Importer } from "../src/analyze";
-import { IndexedFileAnchor, Method, Module } from "../src/lib/famix/src/model/famix";
+import { IndexedFileAnchor, Method, Module, ScriptEntity } from "../src/lib/famix/src/model/famix";
+import GraphemeSplitter from "grapheme-splitter";
 
 const importer = new Importer();
 const project = new Project();
@@ -8,7 +9,6 @@ const project = new Project();
 project.createSourceFile("test_src/simple.ts",
     `let a: number = 1;
 export class A {
-    public b: string = "💷💷💷💷";
     /**
      * Sends the current player back the number of spaces.
      * @param numberOfSpaces The number of spaces to move back.
@@ -17,11 +17,15 @@ export class A {
     public moveBack(numberOfSpaces: number) {
         let currentSquareIndex = this.board.indexOf(this.currentPlayer.currentSquare);
     }
-`, { overwrite: true }).saveSync();
+}`, { overwrite: true }).saveSync();
+
+// multi-code point emoji is handled differently in JavaScript () and Pharo (one character)
+project.createSourceFile("test_src/a-b.ts", `let c = "💷", d = 5;`);
 
 const fmxRep = importer.famixRepFromProject(project);
 
 describe('Tests for source text', () => {
+
     it("should have a Variable 'a' with the proper source text", () => {
         const theFile = Array.from(fmxRep._getAllEntitiesWithType("Module") as Set<Module>)[0];
         const theVariable = Array.from(theFile.getVariables())[0];
@@ -35,17 +39,47 @@ describe('Tests for source text', () => {
                 sourceAnchor.getStartPos() - 1, sourceAnchor.getEndPos() - 1))
                 .toBe("a: number = 1");
     });
+
     it("should have a Variable 'currentSquareIndex' with the proper source text", () => {
         const theMethod = Array.from(fmxRep._getAllEntitiesWithType("Method") as Set<Method>)[0];
         const theVariable = Array.from(theMethod.getVariables())[0];
         const sourceAnchor = theVariable.getSourceAnchor() as IndexedFileAnchor;
         // note: the +1 is because the source anchor is 1-based, but ts-morph is 0-based
-        expect(sourceAnchor.getStartPos()).toBe(355 + 1);
-        expect(sourceAnchor.getEndPos()).toBe(428 + 1);
+        expect(sourceAnchor.getStartPos()).toBe(320 + 1);
+        expect(sourceAnchor.getEndPos()).toBe(393 + 1);
         expect(sourceAnchor.getFileName().endsWith("simple.ts")).toBe(true);
         expect(project.getSourceFileOrThrow(
             sourceAnchor.getFileName()).getFullText().substring(
                 sourceAnchor.getStartPos() - 1, sourceAnchor.getEndPos() - 1))
                 .toBe("currentSquareIndex = this.board.indexOf(this.currentPlayer.currentSquare)");
     });
+
+    const splitter = new GraphemeSplitter();
+    const abFile = Array.from(fmxRep._getAllEntitiesWithType("ScriptEntity") as Set<ScriptEntity>)[0];
+
+    it("should have variable 'c' with the proper source text", () => {
+        expect(abFile).toBeDefined();
+        const theVariable = Array.from(abFile.getVariables())[0];
+        const sourceAnchor = theVariable.getSourceAnchor() as IndexedFileAnchor;
+        const testSourceWithGraphemes = splitter.splitGraphemes('c = "💷"');
+        expect(testSourceWithGraphemes.length).toBe(7);
+        // note: the +1 is because the source anchor is 1-based, but ts-morph is 0-based
+        expect(sourceAnchor.getStartPos()).toBe(4 + 1);
+        expect(sourceAnchor.getEndPos()).toBe(4 + testSourceWithGraphemes.length + 1);
+        expect(sourceAnchor.getFileName().endsWith("a-b.ts")).toBe(true);
+        const sourceFileTextWithGraphemes = splitter.splitGraphemes(project.getSourceFileOrThrow(sourceAnchor.getFileName()).getFullText());
+        expect(sourceFileTextWithGraphemes.slice(sourceAnchor.getStartPos() - 1, sourceAnchor.getEndPos() - 1)).toEqual(testSourceWithGraphemes);
+    });
+
+    it("should have variable 'd' with the proper source text", () => {
+        const theVariable = Array.from(abFile.getVariables())[1];
+        const sourceAnchor = theVariable.getSourceAnchor() as IndexedFileAnchor;
+        // note: the +1 is because the source anchor is 1-based, but ts-morph is 0-based
+        expect(sourceAnchor.getStartPos()).toBe(13 + 1);
+        expect(sourceAnchor.getEndPos()).toBe(18 + 1);
+        expect(sourceAnchor.getFileName().endsWith("a-b.ts")).toBe(true);
+        const sourceFileTextWithGraphemes = splitter.splitGraphemes(project.getSourceFileOrThrow(sourceAnchor.getFileName()).getFullText());
+        const testSourceWithGraphemes = splitter.splitGraphemes('d = 5');
+        expect(sourceFileTextWithGraphemes.slice(sourceAnchor.getStartPos() - 1, sourceAnchor.getEndPos() - 1)).toEqual(testSourceWithGraphemes);    });
+
 });
